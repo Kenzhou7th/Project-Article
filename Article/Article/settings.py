b75ff090142b3ed32a83eeea1b6fd859f551dc77
Maku Kenesu <caumban.kenneth07@gmail.com>
2025-02-28 09:41:38 +0800

    modified models, views, and serializers.

--- Article/Article/settings.py
+++ Article/Article/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'appticle',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- Article/appticle/views.py
+++ Article/appticle/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Post, Comment
+from .serializer import PostSerializer, CommentSerializer
 
 # Create your views here.
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
